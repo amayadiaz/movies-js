c3a0cd87abfa2856089a290c403491512891d1ae
jamaya <julian.amayad@gmail.com>
2019-08-29 10:11:02 -0500

    Clean code and improve Search

--- app.js
+++ app.js
@@ -1,56 +1,46 @@
-
-// VARIABLES 
-
-// Default
+// Default Variables
 const apiUrl = 'https://api.themoviedb.org/3/';
-const keyUrl = 'api_key=84ff3251498b1fa0b9f22832083b3196&page=';
+const apiKeyUrl = 'api_key=84ff3251498b1fa0b9f22832083b3196&page=';
+
 let defaultFavorites = [];
 let defaultName = 'User';
 
-// Elements
+// Elements Variables
 const $articlesList = document.getElementById('articles-list');
 const $modalName = document.querySelector('.modal-name');
-const $username = document.querySelector('.username');
+const $userName = document.querySelector('.username');
 const $inputSearch = document.querySelector('.input-search');
 const $inputName = document.querySelector('.input-name');
 
-// Storage
+// Storage Variables
 let favoritesStorage =  JSON.parse(localStorage.getItem('favorites'));
 let nameStorage =  localStorage.getItem('name');
 
-// END VARIABLES
+// Functions Declarations
+const verifyStorage = (storage, storageItem, defaultValue) => {
+    
+    if(storage == null){
+        localStorage.setItem(storageItem, defaultValue);
+    }else{
+        defaultValue = storage;
+    }
 
-// MAIN
-
-// Assign current storage to variable
-defaultFavorites = verifyStorage(favoritesStorage, 'favorites',  JSON.stringify(defaultFavorites));
-defaultName = verifyStorage(nameStorage, 'name', defaultName);
-
-// Verify if storage variable is default, if is true show the modal to request name
-if(defaultName === 'User'){
-    $modalName.classList.remove('hidden');
+    return defaultValue;
+    
 }
 
-// Assign username value 
-$username.textContent = defaultName;
+const addEnterEvent = (element, button) => {
+    element.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            document.querySelector(button).click();
+        }
+    })
+}
 
+const loadData = (requestUrl, page) => {
 
-// Add event enter to Input Search
-// addEnterEvent($inputSearch, '.button');
-addEnterEvent($inputName, '.button-name');
-
-
-// First Fetch call
-loadData('trending/movie/week?', '1');
-
-// END MAIN
-
-
-// FUNCTIONS 
-
-function loadData(requestUrl, page) {
-
-    fetch(apiUrl + requestUrl + keyUrl + page)
+    fetch(apiUrl + requestUrl + apiKeyUrl + page)
     .then(function(response) {
         return response.json();
     })
@@ -61,8 +51,7 @@ function loadData(requestUrl, page) {
     })
 }
 
-
-function displayArticles(data){
+const displayArticles = (data) => {
 
     data.results.forEach(element => {
         
@@ -74,13 +63,52 @@ function displayArticles(data){
 
 }
 
-
-function assignTemplate(poster, vote_average, id, isFavorite) {
+const assignTemplate = (poster, vote_average, id, isFavorite) => {
     let article = articleTemplate(poster, vote_average, id, isFavorite);
     $articlesList.innerHTML += article; 
 }
- 
-function showTrailer(articleId) {
+
+const articleTemplate = (imagePath, score, idArticle, isFavorite) => {
+    
+    let iconHeart = '<i class="far fa-heart icon"></i>';
+
+    if(isFavorite){
+        iconHeart = '<i class="fas fa-heart icon"></i>';
+    }
+
+    return (
+        `<div class="article grid-item">
+            <div class="cover">
+                <img class="img" src="https://image.tmdb.org/t/p/original${imagePath}" alt="" />
+            </div>
+            <div class="info">
+                <p class="score">${score}<i class="icon fas fa-star"></i></p>
+                <div id="${idArticle}" class="like" onClick="addFavorite(this.id, this)">`+iconHeart+`</span></div>
+                <a id="${idArticle}" onClick="showTrailer(this.id)" class="trailer"><span class="text">Play Trailer</span></a>
+            </div>
+        </div>`
+    )
+}
+
+const searchMovie = () => {
+
+    $articlesList.innerHTML = "";
+
+    const querySearch = document.querySelector('input');
+
+    let requestUrl = ''
+
+    if(querySearch.value != ''){
+        requestUrl = 'search/movie?&query=' + querySearch.value + '&';
+    }else{
+        requestUrl = 'trending/movie/week?';
+    }
+
+    loadData(requestUrl, '1');
+    
+}
+
+const showTrailer = (articleId) => {
     
     const $iframe = document.getElementById('iframe-trailer');
 
@@ -96,7 +124,7 @@ function showTrailer(articleId) {
     $modal.classList.remove('hidden');
 }
 
-function closeTrailer() {
+const closeTrailer = () => {
     const $modal = document.querySelector('.modal');
     $modal.classList.add('hidden');
     const $iframe = document.getElementById('iframe-trailer');
@@ -104,41 +132,7 @@ function closeTrailer() {
 
 }
 
-function searchMovie() {
-
-    $articlesList.innerHTML = "";
-
-    const querySearch = document.querySelector('input');
-
-    let requestUrl = 'search/movie?&query=' + querySearch.value + '&';
-
-    loadData(requestUrl, '1');
-    
-}
-
-function articleTemplate(imagePath, score, idArticle, isFavorite) {
-    
-    let iconHeart = '<i class="far fa-heart icon"></i>';
-
-    if(isFavorite){
-        iconHeart = '<i class="fas fa-heart icon"></i>';
-    }
-
-    return (
-        `<div class="article item">
-            <div class="cover">
-                <img class="img" src="https://image.tmdb.org/t/p/original${imagePath}" alt="" />
-            </div>
-            <div class="info">
-                <p class="score">${score}<i class="icon fas fa-star"></i></p>
-                <div id="${idArticle}" class="like" onClick="addFavorite(this.id, this)">`+iconHeart+`</span></div>
-                <a id="${idArticle}" onClick="showTrailer(this.id)" class="trailer"><span class="text">Play Trailer</span></a>
-            </div>
-        </div>`
-    )
-}
-
-function addFavorite(articleId, element) {
+const addFavorite = (articleId, element) => {
 
     element.childNodes[0].classList.remove('far');
     element.childNodes[0].classList.add('fas');
@@ -148,7 +142,7 @@ function addFavorite(articleId, element) {
 
 }
 
-function showFavorites() {
+const showFavorites = () => {
     $articlesList.innerHTML = '';
 
     favoritesStorage.forEach(element => {
@@ -165,7 +159,7 @@ function showFavorites() {
     });
 }
 
-function nextPage(element) {
+const nextPage = (element) => {
     const page = element.textContent;
 
     $articlesList.innerHTML = "";
@@ -174,38 +168,69 @@ function nextPage(element) {
     
 }
 
-function addName(){
+const addName = () =>{
     
     const newName = document.querySelector('.input-name').value;
-    $username.textContent = newName;
+    $userName.textContent = newName;
     localStorage.setItem('name', newName);
-
 
     $modalName.classList.add('hidden');
 
 }
+// End Functions Declarations
 
-function verifyStorage(storage, storageItem, defaultValue) {
-    
-    if(storage == null){
-        localStorage.setItem(storageItem, defaultValue);
-    }else{
-        defaultValue = storage;
-    }
 
-    return defaultValue;
-    
+// MAIN 
+
+// Assign current storage to variable
+defaultFavorites = verifyStorage(favoritesStorage, 'favorites',  JSON.stringify(defaultFavorites));
+defaultName = verifyStorage(nameStorage, 'name', defaultName);
+
+// Verify if storage variable is default, if is true show the modal to request name
+if(defaultName === 'User'){
+    $modalName.classList.remove('hidden');
 }
 
-function addEnterEvent(element, button) {
-    element.addEventListener("keyup", function(event) {
-        if (event.keyCode === 13) {
-            event.preventDefault();
-            document.querySelector(button).click();
-        }
-    })
-}
-// END FUNCTIONS
+// Assign username value 
+$userName.textContent = defaultName;
+
+
+// Add event enter to Input Search
+addEnterEvent($inputSearch, '.button');
+addEnterEvent($inputName, '.button-name');
+
+$inputSearch.addEventListener("input", function(event) {
+    searchMovie();
+})
+
+// First Fetch call
+loadData('trending/movie/week?', '1');
+
+// END MAIN
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
